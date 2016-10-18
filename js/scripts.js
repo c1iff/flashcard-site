5d27660d7955b6531e1fc94c7ea17ba7f1ace409
Chris Clifford and Ryan Peterson <ryan@>
2016-10-18 16:16:44 -0700

    style the flash card divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".btn").click(function(){
-    $parent_div = $(this).closest('div');
+    var $parent_div = $(this).closest('div');
     $parent_div.find('h5').fadeToggle();
     $parent_div.find('h4').fadeToggle();
   });
